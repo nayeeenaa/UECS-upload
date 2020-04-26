6e71f19e9b05532ae4d34f2164a9e4a9ecbe0c86
sabyasachi-paul-eee <63183052+sabyasachi-paul-eee@users.noreply.github.com>
2020-04-26 17:58:19 +0600

    Controller Operation
    An example of how controller node works with UARDECs

---
+++ controller_thermostat.ino
@@ -0,0 +1,28 @@
+int ChangeThermostat(){
+  signed long showValueTemp;
+  signed long setONOFFAUTO;
+  signed long setONTempFromWeb;
+  signed long showValueStatus;
+  
+    showValueTemp = U_ccmList[CCMID_InAirTemp].value;
+
+  setONOFFAUTO=1;
+
+//サーモスタット動作 
+if(setONOFFAUTO==0)
+    {U_ccmList[CCMID_cnd].value=0;}//Manual OFF
+else if(setONOFFAUTO==1)
+    {U_ccmList[CCMID_cnd].value=1;}//Manual ON
+else if(setONOFFAUTO==2  && U_ccmList[CCMID_InAirTemp].value>32.0)
+    {U_ccmList[CCMID_cnd].value=1;}//Auto ON
+else
+    {U_ccmList[CCMID_cnd].value=0;}//
+
+    showValueStatus = U_ccmList[CCMID_cnd].value;
+
+    Serial.print(showValueStatus);
+
+    return showValueStatus;
+
+    
+}
